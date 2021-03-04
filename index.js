8f329676b3429c2a1b380e97486d013c958abaf0
Nivm200 <Nivm200@gmail.com>
2021-03-04 16:42:53 +0200

    Organized all files
    Added database folder, which inside contains database.json to hold all data, and database.js which is the database class containing all the utils

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+// let submitButton = document.getElementById("submit");
+
+//Adding function to submit button
+// submitButton.addEventListener("click", shortenLink);
+
+function shortenLink() {
+  // let urlText = document.getElementById("url_input").value;
+  if (isURL(urlText)) {
+    let obj = { original_url: urlText, short_url: 1 };
+    var asJSON = JSON.stringify(obj);
+  } else {
+    //Not a url
+  }
+}
+
+function isUrl(text) {
+  if (true) {
+    //Check if text is URL
+    return true;
+  } else {
+    return false;
+  }
+}
